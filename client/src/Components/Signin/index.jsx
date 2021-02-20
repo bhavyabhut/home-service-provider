23f830d78889d51fe72afb13ac347b191387dc8b
Bhavya <bhavya@glabbr.com>
2021-02-20 14:25:47 +0530

    signin and signup page

--- client/src/Components/Signin/index.jsx
+++ client/src/Components/Signin/index.jsx
@@ -1,9 +1,133 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Input, Form, Switch, Divider } from "antd";
+import { Link } from "react-router-dom";
+import {
+  EyeInvisibleOutlined,
+  EyeTwoTone,
+  UnlockOutlined,
+} from "@ant-design/icons";
+import Logo from "../../Layout/Logo";
 
-export default function index() {
-    return (
-        <div>
-            hello
+const SignIn = () => {
+  const [form] = Form.useForm();
+  return (
+    <div className="signin">
+      <div className="signin-form">
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            position: "relative",
+            right: "1rem",
+          }}
+        >
+          <Logo /> <h1 className="title">HomeServices</h1>
         </div>
-    )
-}
+        <h2 className="welcomeBack">Welcome back</h2>
+        <p class="loginIntoAccount">Please log into your account</p>
+        <div>
+          <Form form={form} layout="vertical">
+            <Form.Item
+              label="Email"
+              //   required
+              //   tooltip="This is a required field"
+            >
+              <Input placeholder="johndoe@gmail.com" />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              //   tooltip={{
+              //     title: "Password",
+              //     icon: <InfoCircleOutlined />,
+              //   }}
+            >
+              <Input.Password
+                placeholder="password"
+                iconRender={(visible) =>
+                  visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                }
+              />
+            </Form.Item>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginBottom: "1.5rem",
+                marginTop: "2rem",
+              }}
+            >
+              <div>
+                <Switch></Switch>
+                <span className="remeberMe">Remember me</span>
+              </div>
+              <div>
+                <Link
+                  style={{ color: "rgb(0, 132, 137)" }}
+                  to="/forgot-password"
+                >
+                  Forgot Password?
+                </Link>
+              </div>
+            </div>
+            <Form.Item style={{ width: "100%" }}>
+              <Button
+                type="primary"
+                shape="round"
+                icon={<UnlockOutlined />}
+                style={{
+                  width: "100%",
+                  height: "2.5rem",
+                  backgroundColor: "rgb(0, 132, 137)",
+                  borderColor: "rgb(0, 132, 137)",
+                }}
+                // loading={true}
+              >
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+          <Divider>Or login with</Divider>
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "1fr 1fr",
+              columnGap: "1rem",
+              rowGap: "1rem",
+            }}
+          >
+            <Button type="primary">Facebook</Button>
+            <Button style={{ backgroundColor: "red" }} type="primary">
+              Instagram
+            </Button>
+            <Button style={{ backgroundColor: "red" }} type="primary">
+              Google
+            </Button>
+            <Button type="primary">Github</Button>
+          </div>
+          <p
+            style={{
+              textAlign: "center",
+              margin: "1.5rem 0",
+              fontSize: "1rem",
+              fontWeight: "700",
+              color: "rgb(119, 119, 119)",
+              fontFamily: "Loto",
+            }}
+          >
+            Don't Have an Account?
+            <Link
+              style={{ color: "rgb(0, 132, 137)", marginLeft: "0.5rem" }}
+              to="/signup"
+            >
+              Registration
+            </Link>
+          </p>
+        </div>
+      </div>
+      <div className="signin-image-div" />
+    </div>
+  );
+};
+
+export default SignIn;
