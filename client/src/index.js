8720bb667f9bf6af38213c6f07a8b6c52438cd29
Bhavya Bhut <pbhavy4@gmail.com>
2021-09-17 17:50:56 +0530

    ReactGa

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,9 @@ import { Provider } from "./Context/GlobalContext";
 
 import "./index.css";
 import "antd/dist/antd.css";
+import ReactGA from "react-ga";
+ReactGA.initialize("G-73LXHGW8XW");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
