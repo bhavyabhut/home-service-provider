ea31d6b6bd6c3a45d89589ef626817e0ac71f5a6
Bhavya Bhut <pbhavy4@gmail.com>
2022-12-06 23:42:17 +0530

    add : home page url

--- client/src/router.js
+++ client/src/router.js
@@ -106,7 +106,7 @@ const routes = [
 class Routers extends React.Component {
   render() {
     return (
-      <Router>
+      <Router basename="/">
         <Layout>
           <Suspense fallback={<Spinner />}>
             <Switch render={({ children }) => ({ children })}>
