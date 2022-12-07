87ae325934483eee6a3048cb54167872fa9204c4
Bhavya Bhut <pbhavy4@gmail.com>
2022-12-07 15:42:30 +0530

    fix

--- index.js
+++ index.js
@@ -37,10 +37,11 @@ if (NODE_ENV === "development") {
 }
 
 if (NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, './client', 'build')));
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client', 'build', 'index.html'));
-  })
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 //database connection
