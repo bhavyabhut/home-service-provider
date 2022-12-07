9067199d08a585aa96b10788946cbe28f1360755
Bhavya Bhut <pbhavy4@gmail.com>
2022-12-07 11:22:08 +0530

    fix

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ config.PORT = isDevelopment ? 5000 : (process.env.PORT || 5000)
 config.API_VERSION = isDevelopment ? '/v1' : (process.env.API_VERSION || '/v1')
 config.NODE_ENV = process.env.NODE_ENV
 config.DB_STRING = isDevelopment ? '<YOUR_DB_STRING>' : process.env.DB_STRING
-config.SECREATE = isDevelopment ? 'development' : process.env.DB_STRING
+config.SECREATE = isDevelopment ? 'development' : process.env.SECREATE
 
 console.log(config, 'config');
 module.exports = { config }
