156b32397e0fdaf7c18092ae7e9d78dc5989d71e
Bhavya Bhut <pbhavy4@gmail.com>
2022-12-07 16:00:40 +0530

    fix : base_url

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -5,6 +5,6 @@ const isDevelopment = process.env.NODE_ENV === 'development' ? true : false
 config.NODE_ENV = process.env.NODE_ENV
 config.VERSION = 'v1'
 
-config.API_END_POINT = isDevelopment ? 'http://localhost:5000/' : ''
+config.API_END_POINT = isDevelopment ? 'http://localhost:5000/' : '/'
 
 export default config
