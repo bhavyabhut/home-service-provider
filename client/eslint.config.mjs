61070e9bcdfd3fe1851b4704f22a06667575e1bb
Bhavya Bhut <pbhavy4@gmail.com>
2024-05-02 00:33:50 +0530

    update : babel config (#14)

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,7 +1,6 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
-
 export default [
   {
     languageOptions: {
